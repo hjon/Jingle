9dab2a64daf9ddd10a7a6177ca48fe4b07235b62
Jon Hjelle <jahjelle@gmail.com>
2015-09-11 14:10:52 -0700

    Initial commit

---
+++ Jingle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jingle
+//
+//  Created by Jon Hjelle on 9/11/15.
+//  Copyright © 2015 Jon Hjelle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
