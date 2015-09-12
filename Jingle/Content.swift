f7e1d5d7e73d4d117e70222573e09239a27d7c86
Jon Hjelle <jahjelle@gmail.com>
2015-09-12 13:25:25 -0700

    Add basic session and content implementations

---
+++ Jingle/Content.swift
@@ -0,0 +1,51 @@
+//
+//  Content.swift
+//  Jingle
+//
+//  Created by Jon Hjelle on 9/11/15.
+//  Copyright © 2015 Jon Hjelle. All rights reserved.
+//
+
+enum Disposition: String {
+    case Session = "session"
+    case EarlySession = "early-session"
+}
+
+enum Senders: String {
+    case None = "none"
+    case Both = "both"
+    case Initiator = "initiator"
+    case Responder = "responder"
+}
+
+enum ContentState {
+    case Starting
+    case Unacked
+    case Pending
+    case Active
+    case Rejected
+    case Removed
+}
+
+class Content {
+    unowned let session: Session
+    let creator: Role
+    let name: String
+    let disposition: Disposition
+    var senders: Senders
+    var state: ContentState
+
+    init(session: Session, creator: Role, name: String, senders: Senders = .Both, disposition: Disposition = .Session) {
+        self.session = session
+        self.creator = creator
+        self.name = name
+        self.senders = senders
+        self.disposition = disposition
+
+        if (self.creator == self.session.role) {
+            state = .Starting
+        } else {
+            state = .Pending
+        }
+    }
+}
---
+++ Jingle/Session.swift
@@ -0,0 +1,50 @@
+//
+//  Session.swift
+//  Jingle
+//
+//  Created by Jon Hjelle on 9/11/15.
+//  Copyright © 2015 Jon Hjelle. All rights reserved.
+//
+
+import Foundation
+
+enum Role: String {
+    case Initiator = "initiator"
+    case Responder = "responder"
+}
+
+enum SessionState {
+    case Starting
+    case Pending
+    case Active
+    case Ended
+}
+
+class Session {
+    let initiator: String
+    let responder: String
+    let role: Role
+    let sid: String
+    var state: SessionState
+
+    var peer: String {
+        if role == .Initiator {
+            return responder
+        } else {
+            return initiator
+        }
+    }
+
+    init(initiator: String, responder: String, role: Role, sid: String = NSUUID().UUIDString) {
+        self.initiator = initiator
+        self.responder = responder
+        self.role = role
+        self.sid = sid
+
+        if self.role == .Initiator {
+            state = .Starting
+        } else {
+            state = .Pending
+        }
+    }
+}
