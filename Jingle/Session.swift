a2534aaef10e334473988ac2eaceea4241e45e2b
Jon Hjelle <jahjelle@gmail.com>
2015-09-15 17:17:39 -0700

    Initial implementation of handling incoming remote actions

--- Jingle/Session.swift
+++ Jingle/Session.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2015 Jon Hjelle. All rights reserved.
 //
 
+import Foundation
+
 enum Role: String {
     case Initiator = "initiator"
     case Responder = "responder"
@@ -24,6 +26,13 @@ class Session {
     let role: Role
     let sid: String
     var state = SessionState.Starting
+    private let queue: NSOperationQueue = {
+        let queue = NSOperationQueue()
+        queue.name = "Sesssion.ActionQueue"
+        queue.maxConcurrentOperationCount = 1
+        queue.qualityOfService = .Utility
+        return queue
+    }()
 
     var peer: String {
         if role == .Initiator {
@@ -44,6 +53,40 @@ class Session {
         return true
     }
 
+    private func internalProcessRemoteRequest(request: ActionData) {
+        switch request.action {
+        case .SessionInitiate:
+            if role == .Initiator || state != .Starting {
+                return request.signalBlock(.OutOfOrder)
+            }
+        case .SessionAccept:
+            if role != .Initiator || state != .Pending {
+                return request.signalBlock(.OutOfOrder)
+            }
+        default:
+            break
+        }
+
+        request.signalBlock(.Ack)
+
+        switch request.action {
+        case .SessionInitiate:
+            state = .Pending
+        case .SessionAccept:
+            state = .Active
+        case .SessionTerminate:
+            state = .Ended
+        default:
+            break
+        }
+    }
+
     func processAction(action: ActionData) {
+        let operation = NSBlockOperation() {
+            self.internalProcessRemoteRequest(action)
+        }
+        // TODO: Enum for .Remote and .Local and remap .Normal and .High?
+        operation.queuePriority = .Normal
+        queue.addOperation(operation)
     }
 }
